5927ac80c58a30dc1c4273a4faa4a59b2d9688d0
Gungrave <gungraveheat123@gmail.com>
2024-07-21 21:33:15 -0600

    Frank

--- Controllers/GamesController.cs
+++ Controllers/GamesController.cs
@@ -1,18 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using BackendAPI.Models;
+using Firebase.Storage;
+using Firebase.Auth;
 
 namespace BackendAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class GamesController : Controller
+    public class GamesController : ControllerBase
     {
         private readonly GamesContext _context;
 
@@ -34,6 +37,71 @@ namespace BackendAPI.Controllers
                 Console.WriteLine(ex);
                 return StatusCode(500, "Internal server error");
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostData(
+    [Bind("Id,juego,estado,runN,rejugando,DatosAdicionales,Calificacion,img,fecha_finalizado")]
+    juegos newgame, [FromForm] IFormFile img)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (img != null && img.Length > 0)
+            {
+                using (var archivoSubido = img.OpenReadStream())
+                {
+                    string child = "FotosTest";
+                    string urlArchivo = await SubirArchivo(archivoSubido, img.FileName, child);
+                    newgame.img = urlArchivo;
+                }
+            }
+
+            _context.juegos.Add(newgame);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetDataById), new { id = newgame.Id }, newgame);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDataById(int id)
+        {
+            var game = await _context.juegos.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return Ok(game);
+        }
+
+        // Método para subir archivo a Firebase Storage
+        private async Task<string> SubirArchivo(Stream archivoSubir, string nombreArchivo, string child)
+        {
+            string email = "jorgefranciscocz@gmail.com";
+            string clave = "ContraseñaXDXD";
+            string ruta = "desarolloweb-7ffb8.appspot.com";
+            string apikey = "AIzaSyBbIwF8pmsda6lLtldYsro7e_Aa_SCNGq0";
+
+            var auth = new FirebaseAuthProvider(new FirebaseConfig(apikey));
+            var autentificar = await auth.SignInWithEmailAndPasswordAsync(email, clave);
+            var cancellation = new CancellationTokenSource();
+            var tokenuser = autentificar.FirebaseToken;
+
+            var cargararchivo = new FirebaseStorage(ruta,
+                new FirebaseStorageOptions
+                {
+                    AuthTokenAsyncFactory = () => Task.FromResult(tokenuser),
+                    ThrowOnCancel = true
+                }
+            ).Child(child)
+            .Child(nombreArchivo)
+            .PutAsync(archivoSubir, cancellation.Token);
+
+            var urlcarga = await cargararchivo;
+
+            return urlcarga;
         }
     }
 }
